926d68014ec25895985c1b450e12741b0bf9f3d3
Xavier Luna <xilunacabrera@aggies.ncat.edu>
2018-11-19 20:48:13 -0500

    Good Bye!

---
+++ Goodbye163.java
@@ -0,0 +1,12 @@
+/**
+ *This program will say its final goodbye's
+ *to Comp163.
+ *@author Xavier Isai Luna Cabrera
+ *@param args
+ */
+
+public class Goodbye163 {
+ public static void main(String[] args) {
+  System.out.println("Goodbye COMP 163.");
+  }
+}
